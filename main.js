59325a5cc3a4cc08e644a3593aa9fadbcd47aa78
Natasha Cox <natashac1014@gmail.com>
2018-05-23 10:53:05 -0500

    it worked

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-var list = [];
-for (var i = 1, i <= 100, i++) {
-    return list[i];
+function incrementCounterOutput () {
+    for (i = 1; i <= 100; i ++) {
+      document.getElementById("content").innerHTML += [i];
+           if (i % 7 === 0 && i % 5 === 0) {
+        console.log("ChickenMonkey");
+       }  else if (i % 5 === 0 ) {
+           console.log("Chicken");
+       }  else if (i % 7 === 0) {
+           console.log("monkey")
+       }  else {
+           console.log(i);
+       }
+  }
 }
+
+incrementCounterOutput();
